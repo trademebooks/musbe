92e32e6f83fb35e3a55bb2d80eedb31331b686c9
yichen <yichenzhu1337@gmail.com>
2014-08-11 22:20:51 -0400

    test3

--- app/routes.php
+++ app/routes.php
@@ -77,10 +77,10 @@ Route::get('test', function(){
 	//dd(App::environment());
 	var_dump((getenv('ENV')));
 	var_dump(Config::get('database.default'));
-	var_dump($_ENV['DB_HOST']);
-	var_dump($_ENV['DB_NAME']);
-	var_dump($_ENV['DB_USERNAME']);
-	var_dump($_ENV['DB_PASSWORD']);
+	var_dump(getenv('DB_HOST'));
+	var_dump(getenv('DB_NAME'));
+	var_dump(getenv('DB_USERNAME'));
+	var_dump(getenv('DB_PASSWORD'));
 });
 /*
  * Wildcards all last so you do not overwrite anything up top
